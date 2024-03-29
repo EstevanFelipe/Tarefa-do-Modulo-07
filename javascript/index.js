7803df1ab4848084a1df533edda44e57805230ce
EstevanFelipe <estevanfelipesilva@gmail.com>
2024-03-29 01:14:11 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ const form = document.getElementById('form-confirm');
 const inputA = document.getElementById('valor-a');
 const inputB = document.getElementById('valor-b');
 const errorMessage = document.querySelector('.error-message');
+const successMessage = document.querySelector('.sucess-message');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault(); // Previne o carregamento da página após o "submit"
@@ -11,7 +12,7 @@ form.addEventListener('submit', function (e) {
 
     if (valB > valA) {
         // Formulário válido
-        const successMessage = 'Valor B é maior que Valor A!';
+        const successMessage = 'Valor B é maior que Valor A!'; 
         const containerSuccessMessage = document.querySelector('.success-message');
         containerSuccessMessage.innerHTML = successMessage;
         containerSuccessMessage.style.display = 'block';
@@ -23,5 +24,6 @@ form.addEventListener('submit', function (e) {
     } else {
         // Formulário inválido
         errorMessage.style.display = 'block';
+        successMessage.style.display = 'none';
     }
 });
